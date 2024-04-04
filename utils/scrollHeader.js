2d5d8b17324969866b20f0294c5e64c2d829e3b7
nxhuy1697 <nxhuy1697@gmail.com>
2024-04-04 16:09:18 +0700

    update javascript

---
+++ utils/scrollHeader.js
@@ -0,0 +1,56 @@
+
+// window.onscroll = function () {
+//   if (window.scrollY > 100) {
+//     document.querySelector(".home__header-navbar").classList.add("scroll");
+//   } else{
+//     document.querySelector(".home__header-navbar").classList.remove("scroll");
+//   }
+// };
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('.home__header-navbar .navbar__link a');
+
+// window.onscroll = () => {
+//     let top = window.scrollY;
+
+//     sections.forEach(sec => {
+//         let offset = sec.offsetTop ;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+
+//         if (top >= offset && top < offset + height) {
+//             navLinks.forEach(link => {
+//                 link.classList.remove('active');
+//             });
+//             document.querySelector('.home__header-navbar .navbar__link a[href="#' + id + '"]').classList.add('active');
+//         }
+//     });
+// };
+window.onscroll = function () {
+  if (window.scrollY > 100) {
+    document.querySelector(".home__header-navbar").classList.add("scroll");
+  } else {
+    document.querySelector(".home__header-navbar").classList.remove("scroll");
+  }
+
+  highlightActiveSection(); 
+};
+
+function highlightActiveSection() {
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('.home__header-navbar .navbar__link a');
+  let top = window.scrollY;
+
+  sections.forEach(sec => {
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      document.querySelector('.home__header-navbar .navbar__link a[href="#' + id + '"]').classList.add('active');
+    }
+  });
+}
+
---
+++ utils/scrollSection.js
@@ -0,0 +1,17 @@
+// let sections = document.querySelector('section');
+// let navLinks = document.querySelector('.home__header-navbar .navbar__link a');
+
+// window.onscroll = () => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 150;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute('id');
+//     if(top >= offset && top < offset + height){
+//         navLinks.forEach(links => {
+//             links.classList.remove('active');
+//             document.querySelector('.home__header-navbar .navbar__link a[href*=' + id +']').classList.add('active');
+
+//         })
+//     }
+// }
+
