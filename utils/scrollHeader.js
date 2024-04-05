1fec4462f520c8f2903a6b130f953cc89cd25bc5
nxhuy1697 <nxhuy1697@gmail.com>
2024-04-05 16:36:13 +0700

    update html

--- utils/scrollHeader.js
+++ utils/scrollHeader.js
@@ -1,30 +1,4 @@
 
-// window.onscroll = function () {
-//   if (window.scrollY > 100) {
-//     document.querySelector(".home__header-navbar").classList.add("scroll");
-//   } else{
-//     document.querySelector(".home__header-navbar").classList.remove("scroll");
-//   }
-// };
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('.home__header-navbar .navbar__link a');
-
-// window.onscroll = () => {
-//     let top = window.scrollY;
-
-//     sections.forEach(sec => {
-//         let offset = sec.offsetTop ;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
-
-//         if (top >= offset && top < offset + height) {
-//             navLinks.forEach(link => {
-//                 link.classList.remove('active');
-//             });
-//             document.querySelector('.home__header-navbar .navbar__link a[href="#' + id + '"]').classList.add('active');
-//         }
-//     });
-// };
 window.onscroll = function () {
   if (window.scrollY > 100) {
     document.querySelector(".home__header-navbar").classList.add("scroll");
@@ -37,7 +11,7 @@ window.onscroll = function () {
 
 function highlightActiveSection() {
   let sections = document.querySelectorAll('section');
-  let navLinks = document.querySelectorAll('.home__header-navbar .navbar__link a');
+  let navLinks = document.querySelectorAll('.home__header-navbar .navbar__link a'); // Chỉnh sửa selector ở đây
   let top = window.scrollY;
 
   sections.forEach(sec => {
@@ -53,4 +27,5 @@ function highlightActiveSection() {
     }
   });
 }
+
 
